c98751a4506e2dcc67e72caf72135c54c1e4f647
romain desgeorge <romain.desgeorge@epfedu.fr>
2022-12-07 11:20:49 +0100

    Avancement projet autonome lightsoff

--- src/lightsoff_mallassagne_desgeorge/PlateauDeJeu.java
+++ src/lightsoff_mallassagne_desgeorge/PlateauDeJeu.java
@@ -9,5 +9,28 @@ package lightsoff_mallassagne_desgeorge;
  * @author rom
  */
 public class PlateauDeJeu {
+    public PlateauDeJeu(){
+        for (int i=0;i<5;i++){
+            for (int j=0;j<5;j++){
+                grille[i][j] = new Cellule();
+            }
+        }
+    }
+    Cellule [][] grille = new Cellule[5][5];
     
+    public void affichergrille(){
+        for (int i=4;i>-1;i--){
+            for (int j = 0;j<5;j++){
+                if (grille[i][j].Luminosite() == true){
+                    System.out.print("[OOO]");
+                }
+                if (grille[i][j].Luminosite() == false){
+                    System.out.print("[XXX]");
+                }
+                if (i==4){
+                    System.out.println("\n");
+                }
+            }
+        }
+    }
 }
