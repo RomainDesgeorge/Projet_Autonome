55907e187fe78a39c12a66cf13f5661611e3e539
romain desgeorge <romain.desgeorge@epfedu.fr>
2022-12-07 10:55:17 +0100

    Debut projet autonome lightsoff

---
+++ src/lightsoff_mallassagne_desgeorge/Cellule.java
@@ -0,0 +1,25 @@
+package lightsoff_mallassagne_desgeorge;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author rom
+ */
+public class Cellule {
+    public Cellule(){
+        lumi = lumi;
+    }
+    Lampe lumi = new Lampe();
+    
+    public boolean Luminosite(){
+        boolean verif = false;
+        if (lumi.lumiere == true){
+            verif = true;
+        }
+        return verif;
+    }
+}
---
+++ src/lightsoff_mallassagne_desgeorge/Lightsoff_mallassagne_desgeorge.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lightsoff_mallassagne_desgeorge;
+
+/**
+ *
+ * @author rom
+ */
+public class Lightsoff_mallassagne_desgeorge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
